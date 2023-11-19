d8137815129295c0d2856ea9242c34400f403528
unknown <ifhily15@gmail.com>
2023-11-20 00:57:16 +0400

    first commit

---
+++ Python-Platformer-main/tutorial.py
@@ -0,0 +1,23 @@
+import pygame, sys
+from game import Game
+
+pygame.init()
+dark_blue = (44,44, 127)
+
+screen = pygame.display.set_mode((300, 600))
+pygame.display.set_caption('Heydar HSE Tetris')
+
+clock = pygame.time.Clock()
+
+game = Game()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    screen.fill(dark_blue)
+    game.draw(screen)
+
+    pygame.display.update()
+    clock.tick(60)
